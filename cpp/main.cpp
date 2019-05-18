f3e0eae555f3b8a09db7fba4ae5c16228427c13a
Krzysztof Tomkow <krzysztof.tomkow@gmail.com>
2019-05-18 15:35:40 +0200

    Compiles with opencv

---
+++ cpp/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <opencv4/opencv2/core.hpp>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char** argv)
+{
+    cout << "Hello from main" << endl;
+    cout << "OpenCV version: " << CV_VERSION << endl;
+    cout << "Seems like it works " << endl;
+}
