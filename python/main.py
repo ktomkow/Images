6210ee0404270d12b15d9c8dface9b06ab03cb84
Krzysztof Tomkow <krzysztof.tomkow@gmail.com>
2019-05-19 13:11:03 +0200

    Load, resize, show, save as pgm work

--- python/main.py
+++ python/main.py
@@ -1,5 +1,20 @@
+import cv2
+
 def main():
-    print("Hello")
+    print("Main started")
+
+    img = cv2.imread("fries.jpg", cv2.COLOR_BGR2GRAY)
+
+    grayImage = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    resizedGrayImage = cv2.resize(grayImage,(5,5))
+
+    params = []
+    params.append(cv2.IMWRITE_PXM_BINARY)
+    params.append(0)
+    cv2.imwrite("gray.pgm", resizedGrayImage, params)
+    print("Main finished")
+    
 
 if __name__ == "__main__":
     main()
