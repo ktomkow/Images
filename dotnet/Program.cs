b774fd1fce32467a25ab6addf6271517dd82f8e4
Krzysztof Tomkow <krzysztof.tomkow@gmail.com>
2019-05-19 13:47:15 +0200

    Removed opencvsharp

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-
 namespace dotnet
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var imagePath = "fries.jpg"
             Console.WriteLine("Hello World!");
         }
     }
