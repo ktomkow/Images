996526f6a252deb7b9b246ce9348b341af37ec39
Krzysztof Tomkow <krzysztof.tomkow@gmail.com>
2019-05-21 19:07:45 +0200

    Main publish file

--- dotnet/publish.sh
+++ dotnet/publish.sh
@@ -4,4 +4,10 @@ sudo dotnet restore
 wait
 
 sudo dotnet publish -o ~/Thesis/Testing/Images/dotnet -r linux-x64 -f netcoreapp2.2 --self-contained -c Release
+wait
+
+cd ~/Thesis/Testing/Images/dontet/
+wait
+sudo chmod +x dotnet
+
 echo Done
---
+++ publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd cpp/
+./publish.sh
+wait
+cd ..
+
+cd python/
+./publish.sh
+wait
+cd ..
+
+cd dotnet/
+./publish.sh
+wait
+cd..
+
+echo Done
