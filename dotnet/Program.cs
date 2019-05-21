9458c9fc707a4c705305849dbea8dea9fc2cc073
Krzysztof Tomkow <krzysztof.tomkow@gmail.com>
2019-05-21 16:00:47 +0200

    Image sharp works

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+
 namespace dotnet
 {
     class Program
@@ -11,23 +12,15 @@ namespace dotnet
         {
             var imagePath = "fries.jpg";
             System.Console.WriteLine(imagePath);
-            int width = 128;
-            int height = 128;
-            // Image newImage = Image.FromFile(imagePath);
-            using(FileStream pngStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
-            using(var image = new Bitmap(pngStream))
+
+            using (Image<Rgba32> image = Image.Load(imagePath))
             {
-                var resized = new Bitmap(width, height);
-                using (var graphics = Graphics.FromImage(resized))
-                {
-                    graphics.CompositingQuality = CompositingQuality.HighSpeed;
-                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                    graphics.CompositingMode = CompositingMode.SourceCopy;
-                    graphics.DrawImage(image, 0, 0, width, height);
-                    resized.Save($"resized-{imagePath}", ImageFormat.Png);
-                    Console.WriteLine($"Saving resized-{imagePath} thumbnail");
-                }       
-            } 
+                image.Mutate(x => x
+                    .Resize(image.Width / 2, image.Height / 2)
+                    .Grayscale());
+                image.Save("here.jpg"); // Automatic encoder selected based on extension.
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
