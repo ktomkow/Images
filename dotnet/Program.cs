5b059ec9fef6484a12464c27e3ba05a620c1742c
Krzysztof Tomkow <krzysztof.tomkow@gmail.com>
2019-05-21 15:42:51 +0200

    Works with System.Drawing

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
 namespace dotnet
 {
     class Program
@@ -7,6 +11,23 @@ namespace dotnet
         {
             var imagePath = "fries.jpg";
             System.Console.WriteLine(imagePath);
+            int width = 128;
+            int height = 128;
+            // Image newImage = Image.FromFile(imagePath);
+            using(FileStream pngStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
+            using(var image = new Bitmap(pngStream))
+            {
+                var resized = new Bitmap(width, height);
+                using (var graphics = Graphics.FromImage(resized))
+                {
+                    graphics.CompositingQuality = CompositingQuality.HighSpeed;
+                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    graphics.CompositingMode = CompositingMode.SourceCopy;
+                    graphics.DrawImage(image, 0, 0, width, height);
+                    resized.Save($"resized-{imagePath}", ImageFormat.Png);
+                    Console.WriteLine($"Saving resized-{imagePath} thumbnail");
+                }       
+            } 
             Console.WriteLine("Hello World!");
         }
     }
